11286c8871091aac601676c1f26dbbbe5b7f26ca
Natallia Yurcha <natallia.yurcha@gmail.com>
2021-03-14 20:01:14 +0300

    feat: implement min, max, avg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,40 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+  if (array === undefined || array.length === 0) {
+    return 0;
+  } else {
+    let min = array[0];
+    for (let i = 1; i < array.length; i++) {
+      if (min > array[i]) {
+        min = array[i];
+      }
+    }
+    return min;
+  }
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+  if (array === undefined || array.length === 0) {
+    return 0;
+  } else {
+    let max = array[0];
+    for (let i = 1; i < array.length; i++) {
+      if (max < array[i]) {
+        max = array[i];
+      }
+    }
+    return max;
+  }
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+  if (array === undefined || array.length === 0) {
+    return 0;
+  } else {
+    let sum = 0;
+    for (let i = 0; i < array.length; i++) {
+      sum = sum + array[i];
+    }
+    return sum / array.length;
+  }
 }
